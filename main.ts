ef378b80e67f7bfb6cc8f31dc028fb321c06efa5
ortiqovdev <akmalbekortiqov7976@gmail.com>
2023-01-31 12:29:24 +0500

    funcion sanjarni aqqililiq qilgan masalalari

---
+++ main.ts
@@ -0,0 +1,46 @@
+// type Cylinder = {
+//     r: number
+//     h: number
+// }
+// let cylinder: Cylinder = {
+//     r: 3,
+//     h: 5
+// }
+// let cylinder2: Cylinder = {
+//     r: 3,
+//     h: 15
+// }
+// function volume (cylinder: Cylinder , extra :number){
+//     console.log(cylinder);
+//     let v: number = Math.PI * (cylinder.r ** 2)*cylinder.h
+
+//     return  v + extra
+// }
+// let v1 :number =volume (cylinder , 10)
+// let v2 :number =volume (cylinder2 , 10)
+// console.log(v1);
+// console.log(v2);
+
+// type Rectangele = {
+//     a: number
+//     b: number
+// }
+// let rectangele: Rectangele = {
+//     a: 3,
+//     b: 15
+// }
+// function volume(rectangele: Rectangele) {
+//     let v: number = 2 * (rectangele.a + rectangele.b)
+
+//     return v
+// }
+// let a1: number = volume(rectangele)
+
+// function volume2(rectangele: Rectangele) {
+//     let s: number = (rectangele.a * rectangele.b)
+//     return s
+// }
+// let a2: number = volume2(rectangele)
+
+// console.log(a1);
+// console.log(a2);
